c9e61dd66b44a1dfce6383951069a5c32ad12e3e
Christopher Eaton <chris.eaton@appku.com>
2025-06-19 14:22:58 -0700

    Updated postgres build script to use latest script release.

--- postgres/build.sh
+++ postgres/build.sh
@@ -3,14 +3,14 @@ set -e
 
 if [ ! -f adventureworks.zip ]; then
     echo "Downloading postgres AdventureWorks zip file from github.com/chriseaton/docker-adventureworks";
-    wget "https://github.com/chriseaton/docker-adventureworks/releases/download/2025-05-29/chriseaton-docker-adventureworks-postgres.zip" -O adventureworks.zip -q;
+    wget "https://github.com/chriseaton/docker-adventureworks/releases/download/2025-06-19/chriseaton-docker-adventureworks-postgres.zip" -O adventureworks.zip -q;
     echo "Download complete."
 else
     echo "postgres AdventureWorks file already downloaded. Skipping.";
 fi
 echo "Unzipping postgres AdventureWorks zip file.";
-# unzip adventureworks.zip;
-# rm adventureworks.zip;
+unzip adventureworks.zip;
+rm adventureworks.zip;
 echo "Building postgres docker image.";
 if [[ -f install-a.sql ]] && [[ -f install-a.sql ]] && [[ -f install-a.sql ]] && [[ -f install-a.sql ]]; then
     docker build --platform linux/386,linux/amd64,linux/arm,linux/arm64 --progress=plain . -t chriseaton/adventureworks:postgres -t chriseaton/adventureworks:postgres-16;
